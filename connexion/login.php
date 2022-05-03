2bc6d8179575ad08d2c69e5d331db7ed7edf4b55
Clara Manchon <clara.manchon@etudiant.univ-perp.fr>
2022-05-03 14:44:06 +0200

    ajout des pages de connexion/inscription avec le code php

---
+++ connexion/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Se connecter - Among Us Shop</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <h1><a href="#">AMONG US</a></h1>
+    <hr>
+    <h3>S'identifier</h3>
+    <form method="POST">
+        <label for="identifiant">Identifiant</label><input type="text" name="identifiant" id="identifiant">
+        <label for="password">Mot de passe</label><input type="password" name="password" id="password">
+        <input type="submit" value="Se connecter">
+    </form>
+
+</body>
+</html>
