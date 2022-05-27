e66dc15cd0cf6ac4e8218ad27676da3885885e36
HoudaChemiel <98752510+HoudaChemiel@users.noreply.github.com>
2022-05-27 16:10:54 +0200

    Update script.js
    Rajout Connexion

--- pages/script.js
+++ pages/script.js
@@ -1,3 +1,11 @@
 function fermeNotif() {
     document.getElementById("notif").style.display="none";
 }
+
+function ferme() {
+    document.getElementById("connexion").style.display="none";
+}
+
+function ouvre() {
+    document.getElementById("connexion").style.display="block";
+}
