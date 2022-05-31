0a8adc808d61d8cfe41f7a4b8ca5ee64739109e8
HoudaChemiel <98752510+HoudaChemiel@users.noreply.github.com>
2022-05-31 21:59:48 +0200

    rajout fonction generate PDF

--- pages/script.js
+++ pages/script.js
@@ -9,3 +9,31 @@ function ferme() {
 function ouvre() {
     document.getElementById("connexion").style.display="block";
 }
+
+// variables 
+
+var d = new Date();
+var date = d.getFullYear()+'-'+(d.getMonth()+1)+'-'+d.getDate();
+d.setDate(35);
+var setdate = d.getFullYear()+'-'+(d.getMonth()+1)+'-'+d.getDate();
+
+
+// generer un PDF
+function telecharger(){
+    var nom_fichier = prompt("Nom du fichier PDF :");
+    //generer le pdf
+    var element = document.getElementById('confirmation_paiement');
+    var opt = {
+            margin:  0,
+            filename:     `${nom_fichier}.pdf`,
+            image:        { type: 'jpeg', quality: 1 },
+            html2canvas:  { scale: 2 },
+            jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+        };
+    if(nom_fichier != "") {
+        html2pdf().set(opt).from(element).save();
+    }
+    else {
+        alert("Veuillez choisir un nom ");
+    }
+}
