b403737fb33dad2f0df2d0b0234f8c72510819ab
HoudaChemiel <98752510+HoudaChemiel@users.noreply.github.com>
2022-06-01 09:00:52 +0200

    modif title et copy

--- pages/Boutique_peluche.php
+++ pages/Boutique_peluche.php
@@ -9,7 +9,7 @@
     <script src="https://kit.fontawesome.com/ffb4a8c022.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css"/>
     <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
-    <title>Boutique Peluche</title>
+    <title>Among Us Fan Shop</title>
     <link rel="shortcut icon" type="image/x-icon" href="favicon_io/apple-touch-icon.png"/>
     <?php require("../connexion/open_session.php"); ?>
 </head>
@@ -563,7 +563,7 @@
 
             </div>
 
-            <p class="copyright">&copy; 2022 XXXXXXX.XX - Tous droits réservés.</p>
+            <p class="copyright">&copy; 2022 Among Us Fan Shop - Tous droits réservés.</p>
         </div>
     </footer>
     <!-- FIN Footer -->
